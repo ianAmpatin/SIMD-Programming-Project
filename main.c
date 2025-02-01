a1012b9bc3eb8dda010067a049f9ef8e546ec3e2
ianAmpatin <ian_ampatin@dlsu.edu.ph>
2025-02-01 10:26:58 +0800

    Modified x86

--- main.c
+++ main.c
@@ -17,7 +17,10 @@ uint64_t GetStopWatch()
 	return(uint64_t)(t.QuadPart / (double) freq.QuadPart * NANOSECOND);
 }
 
-extern void x86SIMD(size_t n, double* vector1,double* vector2);
+extern double x86(size_t n, double* vector1,double* vector2);
+extern double AVX1(size_t n, double* vector1, double* vector2);
+extern double AVX2(size_t n, double* vector1, double* vector2);
+
 
 double C_Implementation(size_t n, double* vector1, double* vector2)
 {
@@ -26,18 +29,45 @@ double C_Implementation(size_t n, double* vector1, double* vector2)
 
 int main() {
 	// variables
-	double result;
+	double result = 0.0;
 
 	// array size and bytes required
-	const size_t ARRAY_SIZE = 1 << 24;
+	const size_t ARRAY_SIZE = 1 << 2;
 	const size_t ARRAY_BYTES = ARRAY_SIZE * sizeof(double);
 
 	// array declaration
 	double* vec1 = (double*)malloc(ARRAY_BYTES);
 	double* vec2 = (double*)malloc(ARRAY_BYTES);
 
+	// initialize array values
+	for (int i = 0; i < ARRAY_SIZE; i++)
+	{
+		vec1[i] = 2.0;
+		vec2[i] = 2.0;
+	}
+
+	// print array values
+	for (int i = 0; i < ARRAY_SIZE; i++)
+	{
+		printf("Array 1 : %lf  ||  Array 2 : %lf\n", vec1[i], vec2[i]);
+	}
+
 	//--------------------------- C Program ---------------------------//
 
+	//--------------------------- x86 Program ---------------------------//
+
+	printf("Running x86 Program...\n\n");
+	result = x86(ARRAY_SIZE, vec1, vec2);
+	printf("Result : %lf", result);
+
+	//--------------------------- AVX1 Program ---------------------------//
+
 	//--------------------------- AVX2 Program ---------------------------//
+
+
+	// free arrays
+	free(vec1);
+	free(vec2);
+
 	return 0;
 }
--- x86.asm
+++ x86.asm
@@ -4,4 +4,19 @@ default rel
 global x86
 
 x86:
+
+	XORPS XMM0, XMM0
+	
+	L1:
+
+		MOVSD XMM1, [RDX]
+		MOVSD XMM2, [R8]
+		MULSD XMM1, XMM2
+		ADDSD XMM0, XMM1
+
+		ADD R8, 8
+		ADD RDX, 8
+	
+	LOOP L1
+
 	RET
